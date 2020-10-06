d5adf5b4be6abf405f80f417d82675cd13576041
ItzShekhawat <48554422+ItzShekhawat@users.noreply.github.com>
2020-10-06 09:22:31 +0200

    Update client.py

--- Python/Socket_UDP_First/client.py
+++ Python/Socket_UDP_First/client.py
@@ -1,11 +1,12 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
+ip_server = ""
+server_port = 6000
 
 data = input(">")
 
-s.sendto(data.encode(), ('127.0.0.1', 80))
+s.sendto(data.encode(), (ip_server, server_port))
 
 data , address = s.recvfrom(2048)
 
